138d919bf5808dddef530462129b21c09bdb80bd
Dhadkan <dhadkankc88@gmail.com>
2025-11-28 15:07:17 +0545

    added hpc files

--- week6/index.c
+++ week6/index.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <omp.h>
+
 
 void sayHello() {
     printf("Hello, World from OpenMP!\n");
 }
 
 void main() {
+    
     #pragma omp parallel
     {
         sayHello();
